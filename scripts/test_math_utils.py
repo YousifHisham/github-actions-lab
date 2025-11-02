4b31001c69406866cd74c6832697789936b9d94a
YousifHisham <yousif1242003@gmail.com>
2025-11-02 18:44:55 +0200

    pytest2

--- scripts/test_math_utils.py
+++ scripts/test_math_utils.py
@@ -1,7 +1,8 @@
 from math_utils import add
+from math_utils import subtract
 
 def add_test():
     assert add(2, 3) == 5
 
 def test_subtract():
-    assert add(5, 3) == 2
+    assert test_subtract(5, 3) == 2
